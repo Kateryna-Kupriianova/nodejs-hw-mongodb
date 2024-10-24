c5382a7db377c27c85f1baa361010df0670ff829
Kateryna-Kupriianova <kupriyanov.forever@gmail.com>
2024-10-24 09:57:30 +0300

    change addContactController

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,20 +1,45 @@
-import {isHttpError} from 'http-errors';
+// import {isHttpError} from 'http-errors';
 
 
-export function errorHandler(error, req, res) {
+// export function errorHandler(error, req, res, next) {
 
-    if (isHttpError(error) === true) {
-        return res
-            .status(error.statusCode)
-            .json({ status: error.statusCode, message: error.message,
-            });
-    }
+//     if (isHttpError(error) === true) {
+//         return res
+//             .status(error.statusCode)
+//             .json({ status: error.statusCode, message: error.message,
+//             });
+        
+//     }
 
-    console.error(error);
-      res.status(500).send({
-		status: 500,
-		message: "Something went wrong",
-          data: error || error.message,
-    });
+
+//     console.error(error);
+//       res.status(500).json({
+// 		status: 500,
+// 		message: "Something went wrong",
+//           data: error || error.message,
+//       });
     
+    
+//   }
+
+import { isHttpError } from 'http-errors';
+
+export function errorHandler(error, req, res, next) {
+  if (isHttpError(error)) {
+    res.status(error.statusCode).json({
+      status: error.statusCode,
+      message: error.message,
+    });
+  } else {
+    console.error(error);
+    res.status(500).json({
+      status: 500,
+      message: "Something went wrong",
+      data: error.message || "Unknown error",
+    });
   }
+
+  // Викликаємо next, щоб передати помилку далі, якщо потрібно
+  next(error);
+}
+
--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -3,17 +3,17 @@ import contactController from '../controllers/contactController.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 // import { addContactController } from '../controllers/contactController.js'; 
 const router = express.Router();
-const jsonParser = express.json({
-    type: 'application/json',
-});
+// const jsonParser = express.json({
+//     type: 'application/json',
+// });
 
 router.get('/contacts', ctrlWrapper(contactController.getContacts));
 
 router.get('/contacts/:contactId', ctrlWrapper(contactController.getContactById));
 
-router.post('/contacts', jsonParser, ctrlWrapper(contactController.addContactController));
+router.post('/contacts', express.json(), ctrlWrapper(contactController.addContactController));
 
-router.patch('/contacts/:contactId', jsonParser, ctrlWrapper(contactController.updateContactController));
+router.patch('/contacts/:contactId', express.json(), ctrlWrapper(contactController.updateContactController));
 
 router.delete('/contacts/:contactId', ctrlWrapper(contactController.deleteContactController));
 export default router;
