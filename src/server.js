f298edb0e6b8d147a630c792885b4759e0bb16a9
Kateryna-Kupriianova <kupriyanov.forever@gmail.com>
2024-11-17 20:57:09 +0200

    update rout swagger

--- src/server.js
+++ src/server.js
@@ -15,28 +15,25 @@ import { swaggerDocs } from './middlewares/swaggerDocs.js';
 const PORT = process.env.PORT || 8080;
 
 const setupServer = () => {
-
+  const swaggerUI = require('swagger-ui-express');
   const app = express();
 
+  app.use('/', express.static(path.resolve("src", "public")));
+  app.use("/photos", express.static(path.resolve("src", "public", "photos")));
 
-  app.use("/avatars", express.static(path.resolve("src", "public", "avatars")));
+  app.use('/api/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
   app.use(cookieParser());
-
   app.use(express.json());
+  app.use(cors());
+  app.use(pino());
 
   app.use('/auth', authRoutes);
 
   app.use('/', authenticate, router);
 
-  app.use(cors());
-  app.use(pino());
-
   app.use(notFoundHandler);
   app.use(errorHandler);
-
-  app.use ('/', express.static(path.resolve("src", "public")));
-  app.use('/docs', swaggerDocs);
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
